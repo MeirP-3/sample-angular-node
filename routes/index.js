93217e1bc04f670600326da29b47aa634eedef72
Meir <pechtalt@gmail.com>
2017-02-22 18:52:22 +0200

    k

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index', { title: 'Angular, Node and Twitter API' });
 });
+
 router.get('/table', function(req, res) {
   res.render('table', { title: 'table'});
 });
